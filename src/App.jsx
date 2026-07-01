9b06c7d5755e895acb130a3c5407d0b378c2be9c
Dilshan Silva <sdilshansilva300@gmail.com>
2026-07-01 20:57:44 +0530

    Navbar /Routes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar.component";
 const App = () => {
     return (
-        <h1>MERN Blogging website by modern web</h1>
+       < Routes>
+       <Route path="/" element={<Navbar/>}>
+       <Route path="signin" element={<h1>Sign In page</h1>}/>
+       <Route path="signup" element={<h1>Sign Up page</h1>}/>
+       </Route>
+       </Routes>
     )
 }
-
 export default App;
