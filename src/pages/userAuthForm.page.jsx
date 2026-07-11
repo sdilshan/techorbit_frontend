e0471424b1f7f7b6e40fd0e066364458e8821571
Dilshan Silva <sdilshansilva300@gmail.com>
2026-07-11 22:12:43 +0530

    API integrate and store session data

--- src/pages/userAuthForm.page.jsx
+++ src/pages/userAuthForm.page.jsx
@@ -3,21 +3,53 @@ import InputBox from "../components/input.component";
 import googleIcon from "../imgs/google.png";
 import { Link } from "react-router-dom";
 import AnimationWrapper from "../common/page-animation";
+import { useState } from "react";
+import { signin } from "../services/authService";
+
 
 const UserAuthForm = ({ type }) => {
+  const [formData, setFormData] = useState({
+    fullname: "",
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+  console.log(formData,"This is form data")
+   try {
+    const response = await signin({
+      email: "dilshan@gmail.com",
+      password: "Sdilshan919",
+    });
+
+    console.log("Response:", response);
+  } catch (error) {
+    console.error(error.response?.data || error);
+  }
+  };
   return (
     <AnimationWrapper keyValue={type}>
       <section className="h-cover flex items-center justify-center">
-        <form className="w-[80%] max-w-[400px]">
+        <form className="w-[80%] max-w-[400px]"
+          onSubmit={handleSubmit}>
           <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
             {type === "sign-in" ? "welcome back" : "join us today"}
           </h1>
           {type !== "sign-in" ? (
             <InputBox
-              name="Full Name"
+              name="fullname"
               type="text"
               placeholder="Full Name"
               icon="fi-rs-user"
+              value={formData.fullname}
+              onChange={handleChange}
             />
           ) : (
             ""
@@ -27,14 +59,21 @@ const UserAuthForm = ({ type }) => {
             type="email"
             placeholder="Email"
             icon="fi-rr-envelope"
+            value={formData.email}
+            onChange={handleChange}
           />
           <InputBox
             name="password"
             type="password"
             placeholder="Password"
             icon="fi-rr-key"
+            value={formData.password}
+            onChange={handleChange}
           />
-          <button className="btn-dark center mt-14">
+          <button
+            className="btn-dark center mt-14"
+            type="submit"
+          >
             {type.replace("-", " ")}
           </button>
           <div
