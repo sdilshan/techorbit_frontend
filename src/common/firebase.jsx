c5ee4d4a5ca5e618a1e55304a97dae1658885473
Dilshan Silva <sdilshansilva300@gmail.com>
2026-07-12 12:02:33 +0530

    Move Firebase config to environment variables

--- src/common/firebase.jsx
+++ src/common/firebase.jsx
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC-KiqGyTCp4QwKth_mZim9LJ5vwRhLa4k",
-  authDomain: "techorbit-blog.firebaseapp.com",
-  projectId: "techorbit-blog",
-  storageBucket: "techorbit-blog.firebasestorage.app",
-  messagingSenderId: "411971182596",
-  appId: "1:411971182596:web:85a8999f77ea73cd671551"
+   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
